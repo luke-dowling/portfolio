795c235b2312aec19550df8da07ad0cb7d9a9144
Luke Dowling <lpdowling26@googlemail.com>
2021-04-20 20:13:30 +0200

    [add] change to react app

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "../App.css";
+import { Nav } from "./Nav";
+import { Hero } from "./Hero";
+
+const App = () => {
+  return (
+    <main>
+      <Nav />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+export const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
