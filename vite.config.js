1a5aa1b1a051064188c7033db1b0ca97fbc9cf06
Luke Dowling <lpdowling26@googlemail.com>
2025-09-21 17:48:56 +0200

    add configs to import easier

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgrPlugin(), viteTsconfigPaths()],
   server: {
     open: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@styles/abstracts" as *;`, // Auto-import variables/mixins
+        includePaths: ["src/styles"],
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
   },
 });
