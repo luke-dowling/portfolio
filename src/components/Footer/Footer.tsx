5849189ee3a2117a81a2248c038b8deb1b98db5b
Luke Dowling <lpdowling26@googlemail.com>
2025-09-27 10:43:29 +0200

    update footed styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,8 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <footer>
-      <h3>@2025 luke-dowling</h3>
+    <footer className="footer">
+      <h3 className="footer-heading">@2025 luke-dowling</h3>
       <div className="icons">
         <FaGithub />
         <FaLinkedin />
