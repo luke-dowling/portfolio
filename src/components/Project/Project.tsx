8c718a1d79f885885f2976ec0fe49a1ed050217b
Luke Dowling <lpdowling26@googlemail.com>
2025-09-27 10:43:04 +0200

    update projects, add functional pagination

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import {
   projectsItemAnimation,
   projectsLayoutAnimation,
@@ -6,65 +6,139 @@ import {
 import type { Project as ProjectType } from "@/types";
 import { Link } from "react-router-dom";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { useRef } from "react";
+import { projectsData } from "@/lib/data";
+import { PageTransitionAnimation } from "@/Animations/PageTransitionAnimation";
+
+const xOffset = 100;
+const variants = {
+  enter: (direction: number) => ({
+    x: direction > 0 ? xOffset : -xOffset,
+    opacity: 0,
+    display: "block",
+  }),
+  active: {
+    x: 0,
+    opacity: 1,
+    transition: { delay: 0.2 },
+  },
+  exit: (direction: number) => ({
+    x: direction > 0 ? -xOffset : xOffset,
+    opacity: 0,
+    display: "none",
+  }),
+};
 
 interface ProjectProps {
+  currentProjectId: number;
+  setProject: (newId: number, newDirection?: number) => void;
+  direction: number;
   project: ProjectType;
 }
 
-export const Project = ({ project }: ProjectProps) => {
+export const Project = ({
+  currentProjectId,
+  setProject,
+  direction,
+  project,
+}: ProjectProps) => {
+  const hasPaginated = useRef(false);
+
+  function checkIdIsValid(newId: number) {
+    if (newId === -1) return projectsData.length - 1;
+    if (newId === projectsData.length) return 0;
+    return newId;
+  }
+
+  function detectPaginationGesture(
+    e: MouseEvent | TouchEvent | PointerEvent,
+    { offset }: { offset: { x: number } }
+  ) {
+    if (hasPaginated.current) return;
+    let newId = currentProjectId;
+    const threshold = xOffset / 2;
+
+    if (offset.x < -threshold) {
+      newId = currentProjectId + 1;
+    } else if (offset.x > threshold) {
+      newId = currentProjectId - 1;
+    }
+
+    if (newId !== currentProjectId) {
+      hasPaginated.current = true;
+      // check new id is within bounds
+      newId = checkIdIsValid(newId);
+      setProject(newId, offset.x < 0 ? 1 : -1);
+    }
+  }
+
   return (
-    <motion.div variants={projectsLayoutAnimation}>
-      <motion.h2 className="projects-title" variants={projectsItemAnimation}>
-        {project.title}
-      </motion.h2>
-
-      <Link to={project.url}>
+    <motion.div
+      variants={projectsItemAnimation}
+      className="projects-sliding-container"
+    >
+      <AnimatePresence custom={direction}>
         <motion.div
-          variants={projectsItemAnimation}
-          className="projects-image-container"
+          key={currentProjectId}
+          className="project"
+          data-page={currentProjectId}
+          variants={variants}
+          initial="enter"
+          animate="active"
+          exit="exit"
+          drag="x"
+          onDrag={detectPaginationGesture}
+          onDragStart={() => (hasPaginated.current = false)}
+          onDragEnd={() => (hasPaginated.current = true)}
+          dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+          custom={direction}
         >
-          <FaExternalLinkAlt />{" "}
-          <img
-            srcSet={project.image_mobile}
-            alt={`${project.title} project cover image`}
-          />
+          <motion.h2
+            className="projects-title"
+            variants={projectsItemAnimation}
+          >
+            {project.title}
+          </motion.h2>
+
+          <Link to={project.url}>
+            <motion.div
+              variants={projectsItemAnimation}
+              className="projects-image-container"
+            >
+              <FaExternalLinkAlt className="icon" />{" "}
+              <img
+                srcSet={project.image_mobile}
+                alt={`${project.title} project cover image`}
+              />
+            </motion.div>
+          </Link>
+
+          <motion.h3 variants={projectsItemAnimation}>
+            {project.tagline}
+          </motion.h3>
+          <div className="projects-tech-stack">
+            {project.techStack.map((tech: string) => {
+              return <p key={tech}>{tech}</p>;
+            })}
+          </div>
+          <motion.p
+            variants={projectsItemAnimation}
+            className="project-description"
+          >
+            {project.description}
+          </motion.p>
+
+          {/* <motion.div variants={projectsItemAnimation} className="button-group">
+          <Link to={"/contact"}>
+            <button>Contact</button>
+          </Link>
+
+          <Link to={project.url}>
+            <button>View Project</button>
+          </Link>
+        </motion.div> */}
         </motion.div>
-      </Link>
-
-      <motion.h3 variants={projectsItemAnimation}>{project.tagline}</motion.h3>
-      <div className="projects-tech-stack">
-        {project.techStack.map((tech: string) => {
-          return <p>{tech}</p>;
-        })}
-      </div>
-      <motion.p
-        variants={projectsItemAnimation}
-        className="project-description"
-      >
-        {project.description}
-      </motion.p>
-
-      <motion.div variants={projectsItemAnimation} className="button-group">
-        <Link to={"/contact"}>
-          <button>Contact</button>
-        </Link>
-
-        <Link to={project.url}>
-          <button>View Project</button>
-        </Link>
-      </motion.div>
+      </AnimatePresence>
     </motion.div>
   );
 };
-
-{
-  /* <AnimatePresence>
-    <motion.img
-      key={image.src}
-      src={image.src}
-      initial={{ x: 300, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: -300, opacity: 0 }}
-    />
-  </AnimatePresence> */
-}
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,75 +1,52 @@
 import "./_projects.scss";
 
 import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { PageTransitionAnimation } from "@/Animations/PageTransitionAnimation";
 import { Nav } from "@components/Nav/Nav";
 import { Project } from "@components/Project/Project";
 import { projectsData } from "@/lib/data";
 import { Footer } from "@/components/Footer/Footer";
+import { Pagination } from "@components/Project/Pagination";
+import { projectsItemAnimation } from "@/Animations/projectsAnimations";
+import { useScrollToTop } from "@/hooks/useScrollToTop";
 
 export const Projects = () => {
-  const [index, setIndex] = useState<number>(0);
-  const [selectedProject, setSelectedProject] = useState(projectsData[index]);
+  const pageRef = useRef<HTMLDivElement>(null);
+  useScrollToTop(pageRef);
 
-  function setSlide(newDirection: "left" | "right") {
-    let newIndex: number;
-    if (index === 0 && newDirection === "left") {
-      newIndex = projectsData.length - 1;
-      setIndex(newIndex);
-      return setSelectedProject(projectsData[projectsData.length - 1]);
-    }
-    if (index === projectsData.length - 1 && newDirection === "right") {
-      newIndex = 0;
-      setIndex(newIndex);
-      return setSelectedProject(projectsData[0]);
-    }
+  const [[currentProjectId, direction], setCurrentProjectId] = useState([0, 0]);
+  const [selectedProject, setSelectedProject] = useState(
+    projectsData[currentProjectId]
+  );
 
-    const indexModifier = newDirection === "right" ? 1 : -1;
-    newIndex = index + indexModifier;
-    setIndex(newIndex);
-    setSelectedProject(projectsData[newIndex]);
+  function setProject(newProjectId: number, newDirection?: number) {
+    if (!newDirection) newDirection = newProjectId - currentProjectId;
+    setCurrentProjectId([newProjectId, newDirection]);
+    setSelectedProject(projectsData[newProjectId]);
   }
 
   return (
     <PageTransitionAnimation>
-      <div className="container projects">
+      <motion.div className="container projects" ref={pageRef}>
         <Nav theme="dark" />
-        <h1>
+        <motion.h1 variants={projectsItemAnimation}>
           Projects<span>.</span>
-        </h1>
+        </motion.h1>
 
-        <AnimatePresence custom={index} initial={false} mode="popLayout">
-          <Project key={selectedProject.id} project={selectedProject} />
-        </AnimatePresence>
-
-        <motion.button
-          initial={false}
-          // animate={{ backgroundColor: color }}
-          aria-label="Previous"
-          // style={button}
-          onClick={() => setSlide("left")}
-          // whileFocus={{ outline: `2px solid ${color}` }}
-          whileTap={{ scale: 0.9 }}
-          style={{ backgroundColor: "black" }}
-        >
-          Left
-        </motion.button>
-        <motion.button
-          initial={false}
-          // animate={{ backgroundColor: color }}
-          aria-label="Previous"
-          // style={button}
-          onClick={() => setSlide("right")}
-          // whileFocus={{ outline: `2px solid ${color}` }}
-          whileTap={{ scale: 0.9 }}
-          style={{ backgroundColor: "black" }}
-        >
-          Right
-        </motion.button>
+        <Project
+          currentProjectId={currentProjectId}
+          direction={direction}
+          setProject={setProject}
+          project={selectedProject}
+        />
+        <Pagination
+          currentProjectId={currentProjectId}
+          setProject={setProject}
+        />
         <Footer />
-      </div>
+      </motion.div>
     </PageTransitionAnimation>
   );
 };
