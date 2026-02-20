24db82b0f2de9d3e54ddbf5cacb65148c3bb6332
Luke Dowling <lpdowling26@googlemail.com>
2026-02-20 18:01:09 +0100

    style: restyle footer with top border instead of solid background

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,9 +2,9 @@ import { FaCodepen, FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 export const Footer = () => {
   return (
-    <footer className='relative w-full tablet:w-screen tablet:left-[calc(-50vw+50%)] bg-primary-orange text-primary-white mt-12'>
-      <div className='p-3 flex flex-col gap-2 tablet:max-w-[650px] tablet:mx-auto tablet:flex-row tablet:justify-between tablet:p-5 desktop:max-w-[850px]'>
-        <h3 className='text-primary-white'>@2025 luke-dowling</h3>
+    <footer className='relative w-full tablet:w-screen tablet:left-[calc(-50vw+50%)] border-t-2 border-primary-orange/40 dark:text-primary-white text-primary-black mt-18'>
+      <div className='px-3 py-5 flex flex-col gap-3 tablet:max-w-[650px] tablet:mx-auto tablet:flex-row tablet:justify-between tablet:p-5 desktop:max-w-[850px]'>
+        <h3>@2025 luke-dowling</h3>
         <div className='flex gap-1.5 text-2xl tablet:gap-3 desktop:gap-4'>
           <a href='https://github.com/luke-dowling' target='_blank' rel='noreferrer'>
             <FaGithub />
